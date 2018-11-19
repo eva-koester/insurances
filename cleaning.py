b3410be6100d8a892f15862ef68be0b7bfaa3767
Eva <eva.maria.koester@gmx.de>
2018-11-19 18:54:02 +0100

    extracted data cleaning

--- cleaning.py
+++ cleaning.py
@@ -1,21 +1,21 @@
 # Project: clean the dataset: only one name for each number, drop year-duplicates (2007)
 import pandas as pd
-df = pd.read_csv('beschwerden_all.csv')
-print(len(df))
-
-# remove duplicates of year 2007 from the dataframe: create a new df that only includes year 2007, drop the duplicates,
-# remove all entries with year 2007 from the normal df, coerce the two df's
-df_year_2007 = df[df['Jahr'] == 2007]
-df_year_2007 = df_year_2007.drop_duplicates("Nummer", keep="first")
-df = df[df['Jahr'] != 2007]
-df = pd.concat([df, df_year_2007])
-print(len(df))
-
-# Krankenkassen mit derselben Nummer haben denselben Namen
-grouped = df.groupby('Nummer') ##--> Nummer wird zu name
-for nummer, group in grouped:
-    name = group['Name'].iloc[0]
-    df.loc[df['Nummer'] == nummer, 'Name'] = name
 
 
-print(df.sort_values('Nummer'))
+def load_clean_data():
+    df = pd.read_csv('beschwerden_all.csv')
+    # remove duplicates of year 2007 from the dataframe: create a new df that only includes year 2007, drop the duplicates,
+    # remove all entries with year 2007 from the normal df, coerce the two df's
+    df_year_2007 = df[df['Jahr'] == 2007]
+    df_year_2007 = df_year_2007.drop_duplicates("Nummer", keep="first")
+    df = df[df['Jahr'] != 2007]
+    df = pd.concat([df, df_year_2007])
+    # print(len(df))
+
+    # Krankenkassen mit derselben Nummer haben denselben Namen
+    grouped = df.groupby('Nummer')  ##--> Nummer wird zu name
+    for nummer, group in grouped:
+        name = group['Name'].iloc[0]
+        df.loc[df['Nummer'] == nummer, 'Name'] = name
+    #print(df.sort_values('Nummer'))
+    return df
--- development_complaints.py
+++ development_complaints.py
@@ -2,17 +2,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import cleaning
 
-df = pd.read_csv('beschwerden_all.csv')
+df = cleaning.load_clean_data()
 sns.set(context="talk", style="ticks")
 #sns.set_style("dark")
-
-# remove duplicates of year 2007 from the dataframe: create a new df that only includes year 2007, drop the duplicates,
-# remove all entries with year 2007 from the normal df, coerce the two df's
-df_year_2007 = df[df['Jahr'] == 2007]
-df_year_2007 = df_year_2007.drop_duplicates("Nummer", keep="first")
-df = df[df['Jahr'] != 2007]
-df = pd.concat([df, df_year_2007])
 
 # plot the sum of complaints and insured persons from 2002 to 2017 (not considered: no. of insurances)
 # change new index "Jahr" into according column
@@ -26,4 +20,10 @@ ax2.tick_params('y', colors='red')
 f=sns.lineplot(x='Jahr', y='Versicherte', data=df_sum, ax=ax1)
 g=sns.lineplot(x='Jahr', y='Beschwerden', data=df_sum, ax=ax2, color='red')
 sns.despine(right=False)
+#plt.xticks(rotation=45)
+#g.set_xticklabels(rotation=30)
+#g.set_xticklabels([2008, 2009, 2010], rotation=30)
+#print(g.get_xticklabels())
+#print(f.get_xticklabels())
+#plt.tight_layout()
 plt.show()
